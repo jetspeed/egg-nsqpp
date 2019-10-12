6a730fb2f0bb63e8bb17344a7440209b634de70f
jetspeed <shmimy-w@163.com>
2019-10-12 11:39:25 +0800

    fix config

--- config/config.default.js
+++ config/config.default.js
@@ -6,6 +6,8 @@
  * @property {String} SOME_KEY - some description
  */
 exports.nsqpp = {
+  nsqdHost: '127.0.0.1',
+  nsqdPort: '4150',
   pool: {
     max: 10, // maximum size of the pool
     min: 2, // minimum size of the pool
